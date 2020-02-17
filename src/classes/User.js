6db535c27a0a011672cfb53270ac860c758aba97
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-17 11:18:50 -0700

    Wireframe methods

---
+++ src/classes/User.js
@@ -0,0 +1,55 @@
+class User {
+  constructor(user) {
+    this.id = user.id;
+    this.name = user.name;
+    this.pantry = user.pantry;
+    this.favoriteRecipes = [];
+    this.recipesToCook = [];
+  }
+
+  addFavoriteRecipe(recipe) {
+
+  }
+
+  removeFavoriteRecipe(recipe) {
+
+  }
+
+  addRecipeToCook(recipe) {
+
+  }
+
+  removeRecipeToCook(recipe) {
+
+  }
+
+  filterRecipes(tag) {
+
+  }
+
+  searchRecipes(query) {
+
+  }
+
+
+  // saveRecipe(recipe) {
+  //   this.favoriteRecipes.push(recipe);
+  // }
+  //
+  // removeRecipe(recipe) {
+  //   let i = this.favoriteRecipes.indexOf(recipe);
+  //   this.favoriteRecipes.splice(i, 1);
+  // }
+  //
+  // decideToCook(recipe) {
+  //   this.recipesToCook.push(recipe);
+  // }
+  // filterRecipes(type) {
+  //   return this.favoriteRecipes.filter(recipe => recipe.type.includes(type));
+  // }
+  // searchForRecipe(keyword) {
+  //   return this.favoriteRecipes.filter(recipe => recipe.name.includes(keyword) || recipe.ingredients.includes(keyword));
+  // }
+}
+
+module.exports = User;
