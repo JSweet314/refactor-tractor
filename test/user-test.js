83dff74e47eac92fd2b023255f55e3fd7baaa7ad
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-17 11:53:34 -0700

    Update user tests with mock data and test addFavorite method

--- test/user-test.js
+++ test/user-test.js
@@ -1,44 +1,61 @@
 import { expect } from "chai";
 import User from "../src/classes/User";
+import Recipe from "../src/classes/Recipe";
 import mockUsers from "../data/mock-user-data";
+import mockRecipes from "../data/mock-recipe-data";
 
 describe("User", function() {
   let user;
+  let recipe;
 
   beforeEach(function() {
-    user = new User();
+    const mockUser = mockUsers.wcUsersData[0];
+    user = new User(mockUser);
+    const mockRecipe = mockRecipes.recipeData[0];
+    recipe = new Recipe(mockRecipe)
+  });
+
+  it("should be a function", function() {
+    expect(User).to.be.a('function');
   });
 
   it("should be a an instance of User", function() {
     expect(user).to.be.an.instanceof(User);
   });
 
-  it.skip("should initialize with a name", function() {
-    expect(user.name).to.eq("Saige O'Kon");
-  });
-
-  it.skip("should initialize with an id", function() {
+  it("should initialize with an id", function() {
     expect(user.id).to.eq(1);
   });
 
-  it.skip("should initialize with a pantry", function() {
+  it("should initialize with a name", function() {
+    expect(user.name).to.eq("Saige O'Kon");
+  });
+
+  it("should initialize with a pantry", function() {
     expect(user.pantry[0].ingredient).to.eq(11477);
   });
 
-  it.skip("should initialize with an empty favoriteRecipes array", function() {
+  it("should initialize with an empty favoriteRecipes array", function() {
     expect(user.favoriteRecipes).to.deep.equal([]);
   });
 
-  it.skip("should initialize with an empty recipesToCook array", function() {
+  it("should initialize with an empty recipesToCook array", function() {
     expect(user.recipesToCook).to.deep.equal([]);
   });
 
-  it.skip("should be able to save a recipe to favoriteRecipes", function() {
-    user.saveRecipe(recipe);
-    expect(user.favoriteRecipes[0].name).to.equal("Chicken Parm");
+  it("should be able to save a recipe to favoriteRecipes", function() {
+    user.addFavoriteRecipe(recipe);
+    expect(user.favoriteRecipes[0].id).to.equal(595736);
   });
 
-  it.skip("should be able to decide to cook a recipe", function() {
+  it("shouldn't be able to save recipe if it has already been saved", function() {
+    user.addFavoriteRecipe(recipe);
+    expect(user.favoriteRecipes[0].id).to.equal(595736);
+    expect(user.addFavoriteRecipe.bind(user, recipe)).to.throw(new Error('Recipe already in favorites'))
+    expect(user.favoriteRecipes.length).to.equal(1);
+  });
+
+  it("should be able to decide to cook a recipe", function() {
     user.decideToCook(recipe);
     // expect(user.recipesToCook[0].name).to.equal("Chicken Parm");
   });
