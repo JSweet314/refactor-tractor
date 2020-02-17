93df24ba9964ea6d9e1195608c177cf74de8e9e0
Chris Postma <cjrpostma@gmail.com>
2020-02-17 10:55:54 -0700

    Add incomplete class User tests

--- test/user-test.js
+++ test/user-test.js
@@ -1,61 +1,55 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import User from "../src/classes/User";
+import data from "../data/users-data";
 
-import User from '../src/user';
-import data from '../data/users-data';
-
-describe('User', function() {
+describe("User", function() {
   let user;
-  let userInfo;
-  let recipe;
 
   beforeEach(function() {
-    userInfo = data.users[0];
-    user = new User(userInfo)
-
-    recipe = {name: 'Chicken Parm', type: ['italian', 'dinner']};
+    user = new User();
   });
 
-  it('should be a function', function() {
-    expect(User).to.be.a('function');
+  it("should be a an instance of User", function() {
+    expect(user).to.be.an.instanceof(User);
   });
 
-  it('should initialize with an id', function() {
+  it.skip("should initialize with a name", function() {
+    expect(user.name).to.eq("Saige O'Kon");
+  });
+
+  it.skip("should initialize with an id", function() {
     expect(user.id).to.eq(1);
   });
 
-  it('should initialize with a name', function() {
-    expect(user.name).to.eq('Saige O\'Kon');
-  });
-
-  it('should initialize with a pantry', function() {
+  it.skip("should initialize with a pantry", function() {
     expect(user.pantry[0].ingredient).to.eq(11477);
   });
 
-  it('should initialize with an empty favoriteRecipes array', function() {
+  it.skip("should initialize with an empty favoriteRecipes array", function() {
     expect(user.favoriteRecipes).to.deep.equal([]);
   });
 
-  it('should initialize with an empty recipesToCook array', function() {
+  it.skip("should initialize with an empty recipesToCook array", function() {
     expect(user.recipesToCook).to.deep.equal([]);
   });
 
-  it('should be able to save a recipe to favoriteRecipes', function() {
+  it.skip("should be able to save a recipe to favoriteRecipes", function() {
     user.saveRecipe(recipe);
-    expect(user.favoriteRecipes[0].name).to.equal('Chicken Parm');
+    expect(user.favoriteRecipes[0].name).to.equal("Chicken Parm");
   });
 
-  it('should be able to decide to cook a recipe', function() {
+  it.skip("should be able to decide to cook a recipe", function() {
     user.decideToCook(recipe);
-    expect(user.recipesToCook[0].name).to.equal('Chicken Parm');
+    // expect(user.recipesToCook[0].name).to.equal("Chicken Parm");
   });
 
-  it('should be able to filter recipes by type', function() {
+  it.skip("should be able to filter recipes by type", function() {
     user.saveRecipe(recipe);
-    expect(user.filterRecipes('italian')).to.deep.equal([recipe]);
+    expect(user.filterRecipes("italian")).to.deep.equal([recipe]);
   });
 
-  it('should be able to search recipes by name', function() {
+  it.skip("should be able to search recipes by name", function() {
     user.saveRecipe(recipe);
-    expect(user.searchForRecipe('Chicken Parm')).to.deep.equal([recipe]);
+    expect(user.searchForRecipe("Chicken Parm")).to.deep.equal([recipe]);
   });
 });
