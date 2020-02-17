e07cf83edaff611dc874e7bd7242fa13116df0ac
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-17 14:22:28 -0700

    Write search function

--- src/classes/User.js
+++ src/classes/User.js
@@ -25,11 +25,11 @@ class User {
   }
 
   filterRecipes(searchTags, list) {
-    if (!this[list].length) {
+    if (!list.length) {
       throw new Error(`No recipes saved to ${list}`);
     }
     const filtered = [];
-    this[list].forEach(recipe => {
+    list.forEach(recipe => {
       const inList = searchTags.every(tag => {
         return recipe.tags.includes(tag);
       })
@@ -40,30 +40,17 @@ class User {
     return filtered;
   }
 
-
-
-  searchRecipes(query) {
+  searchRecipes(data, query, list) {
+    query = query.toLowerCase();
+    return list.filter(recipe => {
+      const recipeName = recipe.name.toLowerCase();
+      const recipeIngredients = recipe.ingredients.map(ingredient => {
+        let match = data.find(ingr => ingr.id === ingredient.id);
+        return match.name.toLowerCase();
+      });
+      return recipeName.includes(query) || recipeIngredients.includes(query);
+    });
   }
-
-
-  // saveRecipe(recipe) {
-  //   this.favoriteRecipes.push(recipe);
-  // }
-  //
-  // removeRecipe(recipe) {
-  //   let i = this.favoriteRecipes.indexOf(recipe);
-  //   this.favoriteRecipes.splice(i, 1);
-  // }
-  //
-  // decideToCook(recipe) {
-  //   this.recipesToCook.push(recipe);
-  // }
-  // filterRecipes(type) {
-  //   return this.favoriteRecipes.filter(recipe => recipe.type.includes(type));
-  // }
-  // searchForRecipe(keyword) {
-  //   return this.favoriteRecipes.filter(recipe => recipe.name.includes(keyword) || recipe.ingredients.includes(keyword));
-  // }
 }
 
-module.exports = User;
+export default User;
