2e9ba6c53c3821c247d063d822a07bc79c625a3a
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-18 16:49:33 -0700

    Add displayWelcome method and map out two more methods

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,11 +1,25 @@
 import $ from 'jquery';
 
-const domUpdates = {
+const dom = {
   displayWelcomeMsg: (firstName) => {
     $('#user-name').text(firstName)
-  }
+  },
 
-  
+  displayPantry: (pantry) => {
+
+  },
+
+  createCards: () => {
+    recipeData.forEach(recipe => {
+      let recipeInfo = new Recipe(recipe);
+      // let shortRecipeName = recipeInfo.name;
+      // recipes.push(recipeInfo);
+      // if (recipeInfo.name.length > 40) {
+      //   shortRecipeName = recipeInfo.name.substring(0, 40) + "...";
+      // }
+      addToDom(recipeInfo, shortRecipeName);
+    });
+  }
 }
 
-export default domUpdates;
+export default dom;
