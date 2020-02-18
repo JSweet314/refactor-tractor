333501444a59f20c795d218426d030cffd72916e
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-18 16:19:12 -0700

    Begin communication between index and dom file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,50 @@
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
+import domUpdates from './domUpdates';
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+// globals
+const base = "https://fe-apps.herokuapp.com/api/v1/whats-cookin/1911/";
+const userEndpoint = "users/wcUsersData";
+const ingredientEndpoint = "ingredients/ingredientsData";
+const recipeEndpoint = "recipes/recipeData";
+const randomUserId = getRandomNumber();
+
+function getRandomNumber(min = 1, max = 49) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+// initial fetch of a random user
+// returns a promise of the single user
+const getUser = () => {
+  return fetch(base + userEndpoint)
+    .then(response => response.json())
+    .then(data => data.wcUsersData[randomUserId])
+    .catch(error => console.log(error.message));
+};
+
+const getRecipes = () => {
+  return fetch(base + recipeEndpoint)
+    .then(response => response.json())
+    .then(data => data)
+    .catch(error => console.log(error.message));
+};
+
+// do stuff with the user in this callback
+const currentUser = getUser()
+  .then(user => new User(user))
+
+currentUser
+  .then(user => displayWelcomeMsg('fred'))
+
+// do stuff with the recipes in this callback
+getRecipes().then();
