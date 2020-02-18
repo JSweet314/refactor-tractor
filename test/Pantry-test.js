af40a727ff4d254269e5eb69524b2dd56bef0620
Chris Postma <cjrpostma@gmail.com>
2020-02-17 17:22:46 -0700

    Add additional Pantry tests

--- test/Pantry-test.js
+++ test/Pantry-test.js
@@ -10,7 +10,7 @@ describe("Pantry", function() {
   let user;
   let recipe;
   let pantry;
-  let ingredients;
+  let mockIngredientsData;
 
   beforeEach(function() {
     const mockUser = mockUsers.wcUsersData[0];
@@ -18,7 +18,7 @@ describe("Pantry", function() {
     user = new User(mockUser);
     recipe = new Recipe(mockRecipe);
     pantry = new Pantry(user.pantry);
-    ingredients = mockIngredients.ingredientsData;
+    mockIngredientsData = mockIngredients.ingredientsData;
   });
 
   it("should be a function", function() {
@@ -33,15 +33,34 @@ describe("Pantry", function() {
     expect(pantry.contents).to.deep.equal(user.pantry);
   });
 
-  it("should", function() {});
+  it("should determine if has enough ingredients", function() {
+    expect(pantry.hasEnoughIngredients(recipe)).to.equal(false);
+  });
 
-  it("should", function() {});
+  it("should determine which ingredients user needs more of", function() {
+    expect(
+      pantry.findIngredientsNeeded(recipe, mockIngredientsData)
+    ).to.deep.equal([
+      { name: "wheat flour", amount: 2.5, unit: "c", cost: 142 },
+      { name: "bicarbonate of soda", amount: -0.5, unit: "tsp", cost: 582 },
+      { name: "eggs", amount: -1, unit: "large", cost: 472 },
+      { name: "sucrose", amount: -0.5, unit: "c", cost: 902 },
+      {
+        name: "instant vanilla pudding",
+        amount: -3,
+        unit: "Tbsp",
+        cost: 660
+      },
+      { name: "brown sugar", amount: -0.5, unit: "c", cost: 559 },
+      { name: "salt", amount: -0.5, unit: "tsp", cost: 280 },
+      { name: "fine sea salt", amount: -24, unit: "servings", cost: 528 },
+      { name: "semi sweet chips", amount: -2, unit: "c", cost: 253 },
+      { name: "unsalted butter", amount: -0.5, unit: "c", cost: 617 },
+      { name: "vanilla", amount: -0.5, unit: "tsp", cost: 926 }
+    ]);
+  });
 
-  it("should", function() {});
-
-  it("should", function() {});
-
-  it("should", function() {});
-
-  it("should", function() {});
+  it("should update its contents", function() {
+    expect(pantry.updateIngredients()).to.equal();
+  });
 });
