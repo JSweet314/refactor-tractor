8ff00c8ef4d4ef4de68b43b554cb95db40ad6218
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-20 15:51:39 -0700

    Add super and class extensions

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,7 +1,10 @@
 import Pantry from "./Pantry";
+import RecipeFinder from './RecipeFinder';
 
-class User {
+
+class User extends RecipeFinder {
   constructor(user) {
+    super();
     this.id = user.id;
     this.name = user.name;
     this.pantry = new Pantry(user.pantry);
@@ -26,34 +29,6 @@ class User {
     } else {
       throw new Error(`Recipe doesn't exist in ${list}`);
     }
-  }
-
-  filterRecipes(searchTags, list) {
-    if (!list.length) {
-      throw new Error(`No recipes saved to ${list}`);
-    }
-    const filtered = [];
-    list.forEach(recipe => {
-      const inList = searchTags.every(tag => {
-        return recipe.tags.includes(tag);
-      });
-      if (inList) {
-        filtered.push(recipe);
-      }
-    });
-    return filtered;
-  }
-
-  searchRecipes(data, query, list) {
-    query = query.toLowerCase();
-    return list.filter(recipe => {
-      const recipeName = recipe.name.toLowerCase();
-      const recipeIngredients = recipe.ingredients.map(ingredient => {
-        let match = data.find(ingr => ingr.id === ingredient.id);
-        return match.name.toLowerCase();
-      });
-      return recipeName.includes(query) || recipeIngredients.includes(query);
-    });
   }
 }
 
