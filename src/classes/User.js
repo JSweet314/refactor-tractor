175172c7a8fd7c9fa2d810624d10a786ddd8b248
Lili Manrique <liliana.manrique1@gmail.com>
2020-02-21 16:11:54 -0700

    Update user class to parse local storage data

--- src/classes/User.js
+++ src/classes/User.js
@@ -8,8 +8,8 @@ class User extends RecipeFinder {
     this.id = user.id;
     this.name = user.name;
     this.pantry = new Pantry(user.pantry);
-    this.favoriteRecipes = [];
-    this.recipesToCook = [];
+    this.favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
+    this.recipesToCook = JSON.parse(localStorage.getItem('recipesToCook')) || [];
   }
 
   addRecipe(recipe, list) {
